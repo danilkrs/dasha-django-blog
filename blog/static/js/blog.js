b0165fcb94b8b0f9334ce9499266b71a0d3d8e3c
danil <danil.kr811@gmail.com>
2017-04-25 01:28:10 +0300

    Пофиксил баг в форме комента, редизайн

--- blog/static/js/blog.js
+++ blog/static/js/blog.js
@@ -1,15 +1,15 @@
 $(function(){
 	if($('input[name="author"]').val() !== '') {
 			$('input[name="author"]').css('background', '#c9a96e');
-			$('input[name="author"]').css('color', 'white');
+			$('input[name="author"]').css('color', '#000');
 	}
 	if($('select').children(':selected').val() !== '') {
 			$('select').css('background', '#c9a96e');
-			$('select').css('color', 'white');
+			$('select').css('color', '#000');
 	}
 	if($('textarea').val() !== '') {
 			$('textarea').css('background', '#c9a96e');
-			$('textarea').css('color', 'white');
+			$('textarea').css('color', '#000');
 	}
 	$('input').keypress(function(){
 		var $this = $(this);
@@ -17,24 +17,26 @@ $(function(){
 	});
 	$('input[name="author"]').change(function(){
 		var $this = $(this);
-		if($this.val() !== '') {
-			$this.css('background', '#c9a96e');
-			$this.css('color', 'white');
-		} else {
-			$this.css('background', '#D3D9D9');
-			$this.css('color', '#647373 !important');
-		}
+		var value = Boolean($this.val());
+		if(value) {
+            $this.css('background', '#c9a96e');
+            $this.css('color', '#fff');
+        } else {
+            $this.css('background', '#D3D9D9');
+            $this.css('color', '#000');
+        }
 	});
 	$('input[name="author"]').keydown(function(e){
 		var $this = $(this);
+		var value = $this.val();
 		var keyCode = e.keyCode || e.which; 
 		if (keyCode == 9) { 
-			if($this.val() !== '') {
+			if(value) {
 				$this.css('background', '#c9a96e');
-				$this.css('color', 'white !important');
+				$this.css('color', '#000');
 			} else {
 				$this.css('background', '#D3D9D9');
-				$this.css('color', '#647373 !important');
+				$this.css('color', '#647373');
 			}
 		}
 	});
@@ -51,24 +53,26 @@ $(function(){
 	});
 	$('textarea').change(function(){
 		var $this = $(this);
-		if($this.val() !== '') {
+		var value = $this.val();
+		if(value) {
 			$this.css('background', '#c9a96e');
 			$this.css('color', 'white');
 		} else {
 			$this.css('background', '#D3D9D9');
-			$this.css('color', '#647373 !important');
+			$this.css('color', '#647373');
 		}
 	});
 	$('textarea').keydown(function(e){
 		var $this = $(this);
+		var value = $this.val();
 		var keyCode = e.keyCode || e.which; 
 		if (keyCode == 9) { 
-			if($this.val() !== '') {
+			if(value) {
 				$this.css('background', '#c9a96e');
-				$this.css('color', '#fff !important');
+				$this.css('color', '#fff');
 			} else {
 				$this.css('background', '#D3D9D9');
-				$this.css('color', '#647373 !important');
+				$this.css('color', '#647373');
 			}
 		}
 	});
